41033432a07e97d1ea9a451010d7c9b9dba63328
ffpalu <federico.palumbo997@edu.unito.it>
2025-08-25 11:38:20 +0200

    initial commit

---
+++ src/main/java/com/backend/mentora/MentoraApplication.java
@@ -0,0 +1,13 @@
+package com.backend.mentora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/mentora/MentoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.mentora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
