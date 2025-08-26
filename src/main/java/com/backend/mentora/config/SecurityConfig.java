d18a180a157c782f0f7496cd0ae445f5125e7f48
ffpalu <federico.palumbo997@edu.unito.it>
2025-08-26 16:58:49 +0200

    Fase 2 - Update SecurityConfig with new filterchain and authentication

--- src/main/java/com/backend/mentora/config/SecurityConfig.java
+++ src/main/java/com/backend/mentora/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.backend.mentora.config;
 
 
+import com.backend.mentora.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -16,8 +22,10 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 
 	@Bean
@@ -26,18 +34,24 @@ public class SecurityConfig {
 	}
 
 	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+		return config.getAuthenticationManager();
+	}
+
+	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-						.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-						.csrf(csrf -> csrf.disable())
-						.authorizeHttpRequests( auth -> auth
-										.requestMatchers("/h2-console/**").permitAll()
-										.requestMatchers("/api/auth/**").permitAll()
-										.requestMatchers("/swagger-ui/**", "/api-docs/**").permitAll()
-										.anyRequest().permitAll()
-						)
-						.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin())
-						);
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests( auth -> auth
+							.requestMatchers("/h2-console/**").permitAll()
+							.requestMatchers("/api/auth/**").permitAll()
+							.requestMatchers("/swagger-ui/**", "/api-docs/**").permitAll()
+							.anyRequest().authenticated()
+			)
+			.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 
@@ -45,12 +59,7 @@ public class SecurityConfig {
  	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of(
-						"http://localhost:3000",
-						"http://localhost:5173",
-						"http://localhost:8080"
-		));
-
+		configuration.setAllowedOrigins(List.of("http://localhost:*"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
