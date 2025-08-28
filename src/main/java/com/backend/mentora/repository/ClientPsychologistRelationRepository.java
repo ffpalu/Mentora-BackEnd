301931d0d9b911a11e3b02b2b17023b1e5636a3e
Federico Francesco Palumbo <55998277+ffpalu@users.noreply.github.com>
2025-08-28 16:41:32 +0200

    Fase 5 - Adding repository method

--- src/main/java/com/backend/mentora/repository/ClientPsychologistRelationRepository.java
+++ src/main/java/com/backend/mentora/repository/ClientPsychologistRelationRepository.java
@@ -1,9 +1,16 @@
 package com.backend.mentora.repository;
 
+import com.backend.mentora.entity.Client;
 import com.backend.mentora.entity.ClientPsychologistRelation;
+import com.backend.mentora.entity.Psychologist;
+import com.backend.mentora.entity.enums.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClientPsychologistRelationRepository extends JpaRepository<ClientPsychologistRelation, Long> {
+	List<ClientPsychologistRelation> findByClientAndIsActive(Client client, Boolean isActive);
+	List<ClientPsychologistRelation> findByPsychologistAndStatus(Psychologist psychologist, RequestStatus status);
 }
